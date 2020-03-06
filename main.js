2703b6c023743a381d97529c45927ce863b00ea1
ShreyashNandgaonkar <shreyash.nandgaonkar7@gmail.com>
2020-03-06 15:57:49 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,31 @@
+// // const tabItems = document.querySelectorAll('.nav-item');
+// // const tabContentItems = document.querySelectorAll('.tab-content-item');
+
+// // //nav-item= tab-item
+// // //tab-border=current
+
+// // function selectItem(e) {
+
+// //     removeBorder();
+// //     removeShow();
+// //     this.classList.add('current');
+// //     const tabContentItem = document.querySelector(`#${this.id}-item`);
+// //     tabContentItem.classList.add('show');
+// // }
+
+// // function removeBorder() {
+// //     tabItems.forEach(item => item.classList.remove('current'));
+// // }
+
+// // function removeShow() {
+// //     tabContentItems.forEach(item => item.classList.remove('show'));
+// // }
+// // tabItems.forEach(item => item.addEventListener('click', selectItem));
+
+
+// $(document).ready(function() {
+//     $(".menu-nav a").click(function() {
+//         $(".menu-nav a").removeClass("active");
+//         $(this).addClass("active");
+//     });
+// });
